dc77163a7be03bb112d39d89102e788fae9ebfa4
Juan Giacosa <juangiacosa@MacBook-Air-de-Juan.local>
2021-04-29 13:34:56 -0300

    - Implementada funcionalidad listar clientes - Creada carpeta menu/choices para la obtención de opciones a presentar en menus choice - Generado botón vuelta atrás

--- functions/src/handlers/menus/general.ts
+++ functions/src/handlers/menus/general.ts
@@ -3,6 +3,14 @@ import {createBackMainMenuButtons} from "telegraf-inline-menu";
 import {ExtendedContext} from "../../../config/context/myContext";
 
 export const botonesVueltaAtras = createBackMainMenuButtons<ExtendedContext>(
-  (context) => "Volver 🔙",
-  (context) => "Ir al menu principal"
+  (context) => "🔙 Volver ",
+  (context) => "⏮️ Ir al menu inicial"
+);
+
+/**
+ * Solo genera el botón para volver al menú inicial, sin el botón para navegar un nivel en el arbol
+ */
+export const botonVueltaInicio = createBackMainMenuButtons<ExtendedContext>(
+  undefined,
+  (context) => "⏮️ Ir al menu inicial"
 );
